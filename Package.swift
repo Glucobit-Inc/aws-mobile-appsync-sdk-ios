82b594357635b35ab04739e8ba258c023ba2ee0d
Sanju Varghese <sanv725@hotmail.com>
2026-04-15 18:19:21 -0700

    Set to use exactly 0.13.2 for sqlite

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/stephencelis/SQLite.swift.git",
-            .upToNextMinor(from: "0.15.0")
+            exact: "0.13.2"
         )
     ],
     targets: [
