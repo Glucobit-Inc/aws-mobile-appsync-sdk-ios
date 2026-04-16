3aaea852634e2421ed58282b99890a257c955c1f
Sanju Varghese <sanv725@hotmail.com>
2026-04-15 19:45:51 -0700

    Update to 0.16.0 for sqlite

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/stephencelis/SQLite.swift.git",
-            .upToNextMinor(from: "0.13.2")
+            .upToNextMinor(from: "0.16.0")
         )
     ],
     targets: [
